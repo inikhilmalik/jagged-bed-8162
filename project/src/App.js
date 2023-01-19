2ebb80f75ebb6b2e2371eb250942ed871d154492
Nikhil <inikhilmalik@gmail.com>
2023-01-19 19:58:48 +0530

    day2

--- project/src/App.js
+++ project/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Cards from './Components/Cards';
+import MidSection from './Components/MidSection';
 import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <MidSection/>
+      
     </div>
   );
 }
