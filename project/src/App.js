8436c4868daa4b2f3a4a1c77659440638c65879d
Nikhil <inikhilmalik@gmail.com>
2023-01-20 17:41:21 +0530

    day3

--- project/src/App.js
+++ project/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import Cards from './Components/Cards';
+import Login from './Components/Login';
 import MidSection from './Components/MidSection';
 import Navbar from './Components/Navbar';
+import SignUp from './Components/SignUp';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <MidSection/>
+      {/* <Login/> */}
+      {/* <SignUp/> */}
+      {/* <MidSection/> */}
       
     </div>
   );
--- project/src/Components/MidSection.jsx
+++ project/src/Components/MidSection.jsx
@@ -191,7 +191,7 @@ export default function MidSection() {
             
             <Box textAlign={"start"} margin={"auto"} width={"90%"} marginTop={"30px"}>
                 <Text fontWeight={500} fontSize={35}>Inspire and Get Inspired</Text>
-                <Box marginBottom={"30px"} display={"flex"}  >
+                <Box display={"flex"}  >
                     <Box><Cards image={"https://ii1.pepperfry.com/media/wysiwyg/banners/Hp_section7_web_04012023_1.jpg" }
                     price={"Starting Rs.14499"} title={"Premium Office Chairs"} text={"06+Options"} /></Box>
                     <Box><Cards image={"https://ii2.pepperfry.com/media/wysiwyg/banners/Hp_section7_web_04012023_2.jpg" }
@@ -203,6 +203,217 @@ export default function MidSection() {
                     
                 </Box>
             </Box>
+
+            <Box margin={"auto"} textAlign={"start"} width={"90%"} marginTop={"30px"}>
+                <Text fontWeight={500} fontSize={35}>Trends In Furniture And Decor</Text>
+                <Box display={"flex"}  margin={"auto"} marginTop={"20px"}  >
+                    <Box >
+                        <img width={"97%"} src="https://ii1.pepperfry.com/media/wysiwyg/banners/Hp_section6_web_04012023_1.jpg" alt="pic" />
+                        <Text  marginTop={"10px"} fontSize={22}>Pantone Color Sofas</Text>
+                        <Text  color={"grey"} fontSize={18}>Explore Starting Rs.10939</Text>
+                    </Box>
+                    <Box>
+                        <img width={"97%"} src="https://ii2.pepperfry.com/media/wysiwyg/banners/Hp_section6_web_04012023_2.jpg" alt="" />
+                        <Text marginTop={"10px"} fontSize={22}>Gentle Lighting Shelf Lamps</Text>
+                        <Text  color={"grey"} fontSize={18}>Explore Starting Rs.1999</Text>
+                    </Box>
+                    <Box>
+                        <img width={"97%"} src="https://ii3.pepperfry.com/media/wysiwyg/banners/Hp_section6_web_04012023_3.jpg" alt="" />
+                        <Text marginTop={"10px"} fontSize={22}>Maximalist Pieces By Chumbak</Text>
+                        <Text  color={"grey"} fontSize={18}>Explore Starting Rs.4495</Text>
+                    </Box>
+                </Box>
+            </Box>
+
+            <Box margin={"auto"} textAlign={"start"} width={"90%"} marginTop={"30px"}>
+                <Text fontWeight={500} fontSize={35}>New Arrivals</Text>
+                <Box display={"flex"}  margin={"auto"} marginTop={"20px"}  >
+                    <Box >
+                        <img width={"95%"} src="https://ii1.pepperfry.com/media/wysiwyg/banners/Hp_section7_web_04012023_1.jpg" alt="pic" />
+                        <Text  marginTop={"10px"} fontSize={22}>Premium Office Chairs</Text>
+                        <Text  color={"grey"} fontSize={18}>06+Options</Text>
+                        <Text  color={"grey"} fontSize={18}>Starting Rs.14499</Text>
+                    </Box>
+                    <Box>
+                        <img width={"95%"} src="https://ii1.pepperfry.com/media/wysiwyg/banners/Hp_newarrival_17012023_web.jpg" alt="" />
+                        <Text marginTop={"10px"} fontSize={22}>Unique Collectibles</Text>
+                        <Text  color={"grey"} fontSize={18}>120+Options</Text>
+                        <Text  color={"grey"} fontSize={18}>Starting Rs.249</Text>
+                    </Box>
+                    <Box>
+                        <img width={"95%"} src="https://ii3.pepperfry.com/media/wysiwyg/banners/Hp_section7_web_04012023_3.jpg" alt="" />
+                        <Text marginTop={"10px"} fontSize={22}>Printed Cushion Covers</Text>
+                        <Text  color={"grey"} fontSize={18}>150+Options</Text>
+                        <Text  color={"grey"} fontSize={18}>Starting Rs.149</Text>
+                    </Box>
+                    <Box>
+                        <img width={"95%"} src="https://ii3.pepperfry.com/media/wysiwyg/banners/Home_Deal19_web_19012023_4.jpg" alt="" />
+                        <Text marginTop={"10px"} fontSize={22}>Upholstered Beds</Text>
+                        <Text  color={"grey"} fontSize={18}>70+Options</Text>
+                        <Text  color={"grey"} fontSize={18}>Starting Rs.9199</Text>
+                    </Box>
+                </Box>
+            </Box>
+
+            <Box margin={"auto"} textAlign={"start"} width={"90%"} marginTop={"30px"}>
+                <Text fontWeight={500} fontSize={35}>The Winter Edit</Text>
+                <Box display={"flex"}  margin={"auto"} marginTop={"20px"}  >
+                    <Box >
+                        <img width={"97%"} src="https://ii2.pepperfry.com/media/wysiwyg/banners/Hp_section8_web_04012023_1.jpg" alt="pic" />
+                    </Box>
+                    <Box>
+                        <img width={"97%"} src="https://ii3.pepperfry.com/media/wysiwyg/banners/Hp_section8_web_04012023_2.jpg" alt="" />
+                    </Box>
+                    <Box>
+                        <img width={"97%"} src="https://ii1.pepperfry.com/media/wysiwyg/banners/Hp_section8_web_04012023_3.jpg" alt="" />
+                    </Box>
+                </Box>
+            </Box>
+
+            <Box margin={"auto"} textAlign={"start"} width={"90%"} marginTop={"30px"}>
+                <Text fontWeight={500} fontSize={35}>What The Fry Deals</Text>
+                <Box display={"flex"}  margin={"auto"} marginTop={"20px"}  >
+                    <Box >
+                        <img width={"95%"} src="https://ii2.pepperfry.com/media/wysiwyg/banners/Hp_section9_web_04012023_1.jpg" alt="pic" />
+                        <Text  marginTop={"10px"} fontSize={22}>Popular For Sofas</Text>
+                        <Text  color={"grey"} fontSize={18}>600+Options</Text>
+                    </Box>
+                    <Box>
+                        <img width={"95%"} src="https://ii3.pepperfry.com/media/wysiwyg/banners/Hp_section9_web_04012023_2.jpg" alt="" />
+                        <Text marginTop={"10px"} fontSize={22}>Popular For Office</Text>
+                        <Text  color={"grey"} fontSize={18}>45+Options</Text>
+                    </Box>
+                    <Box>
+                        <img width={"95%"} src="https://ii1.pepperfry.com/media/wysiwyg/banners/Hp_section9_web_04012023_3.jpg" alt="" />
+                        <Text marginTop={"10px"} fontSize={22}>Popular For Home</Text>
+                        <Text  color={"grey"} fontSize={18}>290+Options</Text>
+                    </Box>
+                    <Box>
+                        <img width={"95%"} src="https://ii2.pepperfry.com/media/wysiwyg/banners/Hp_section9_web_04012023_4.jpg" alt="" />
+                        <Text marginTop={"10px"} fontSize={22}>Popular For Serveware</Text>
+                        <Text  color={"grey"} fontSize={18}>120+Options</Text>
+                    </Box>
+                </Box>
+            </Box>
+            <Box margin={"auto"} textAlign={"start"} width={"90%"} marginTop={"30px"}>
+                <Text fontWeight={500} fontSize={35}>Inspiration From Our Blog</Text>
+                <Box display={"flex"}  margin={"auto"} marginTop={"20px"}  >
+                    <Box >
+                        <img width={"95%"} src="https://ii3.pepperfry.com/media/wysiwyg/banners/Hp_section10_web_04012023_1.jpg" alt="pic" />
+                        <Text  color={"grey"} fontSize={18}>How  to Declutter Your House Based on Your Personal Type?</Text>
+                    </Box>
+                    <Box>
+                        <img width={"95%"} src="https://ii1.pepperfry.com/media/wysiwyg/banners/Hp_section10_web_04012023_2.jpg" alt="" />
+                        <Text  color={"grey"} fontSize={18}>5 Elegant Ideas for Your Dreamy Modern Master Bedroom</Text>
+                    </Box>
+                    <Box>
+                        <img width={"95%"} src="https://ii2.pepperfry.com/media/wysiwyg/banners/Hp_section10_web_04012023_3.jpg" alt="" />
+                        <Text  color={"grey"} fontSize={18}>7 Decor Ideas to Spruce up Your Hallway</Text>
+                    </Box>
+                    <Box>
+                        <img width={"95%"} src="https://ii3.pepperfry.com/media/wysiwyg/banners/Hp_section10_web_04012023_4.jpg" alt="" />
+                        <Text  color={"grey"} fontSize={18}>3 Trends You Can Use This Holiday Season In Your Home</Text>
+                    </Box>
+                </Box>
+            </Box>
+
+            <Box margin={"auto"} textAlign={"start"} width={"90%"} marginTop={"30px"}>
+                <Text fontWeight={500} fontSize={35}>Customers Speak</Text>
+                <Box display={"flex"}  margin={"auto"} marginTop={"20px"}  >
+                    <Box >
+                        <img width={"95%"} src="https://ii1.pepperfry.com/media/wysiwyg/banners/Testimonial_web_261221_7.jpg" alt="pic" />
+                        <Text  marginTop={"10px"} fontSize={22}>Bought these two grey puffy stools from pepperfry. Found them to be surprise...</Text>
+                        <br/>
+                        <Text   fontSize={18}>Suman Senapatia</Text>
+                        <Text  color={"grey"} fontSize={18}>Visakhapatnam</Text>
+                    </Box>
+                    <Box >
+                        <img width={"95%"} src="https://ii3.pepperfry.com/media/wysiwyg/banners/happy_customer_block_2_2906_0222.png" alt="" />
+                        <Text marginTop={"10px"} fontSize={22}>I purchased Candy Study Chair in Pink Colour by Alex Daisy from pepperfry looks...</Text>
+                        <br/>
+                        <Text fontSize={18}>Satya Madhana</Text>
+                        <Text  color={"grey"} fontSize={18}>Secunderabad</Text>
+                    </Box>
+                    <Box>
+                        <img width={"95%"} src="https://ii2.pepperfry.com/media/wysiwyg/banners/Testimonial_web_261221_9.jpg" alt="" />
+                        <Text marginTop={"10px"} fontSize={22}>Purchased an ergonomic chair, I thought it would not be fitting for me but once it got...</Text>
+                        <br/>
+                        <Text  fontSize={18}>Baba Kattubadi</Text>
+                        <Text  color={"grey"} fontSize={18}>Hyderabad</Text>
+                    </Box>
+                </Box>
+            </Box>
+
+            <Box margin={"auto"} textAlign={"start"} width={"90%"} marginTop={"30px"}>
+                <Text fontWeight={500} fontSize={35}>Need Help Buying?</Text>
+                <Box display={"flex"}  margin={"auto"} marginTop={"20px"}  >
+                    <Box >
+                        <img width={"95%"} src="https://ii1.pepperfry.com/media/wysiwyg/banners/hp_need_help_p_1.jpg" alt="pic" />
+                        <Text   fontSize={18}>Here's How To Choose The Right Sofa</Text>
+                    </Box>
+                    <Box>
+                        <img width={"95%"} src="https://ii2.pepperfry.com/media/wysiwyg/banners/hp_need_help_p_2.jpg" alt="" />
+                        <Text   fontSize={18}>How To Buy A Perfect Bed For Your Room</Text>
+                    </Box>
+                    <Box>
+                        <img width={"95%"} src="https://ii3.pepperfry.com/media/wysiwyg/banners/hp_need_help_p_3.jpg" alt="" />
+                        <Text   fontSize={18}>Your Guide To Buy The</Text>
+                    </Box>
+                    <Box>
+                        <img width={"95%"} src="https://ii1.pepperfry.com/media/wysiwyg/banners/hp_need_help_p_4.jpg" alt="" />
+                        <Text   fontSize={18}>Everything About Cabinet</Text>
+                    </Box>
+                    <Box >
+                        <img width={"95%"} src="https://ii2.pepperfry.com/media/wysiwyg/banners/hp_need_help_p_5.jpg" alt="pic" />
+                        <Text   fontSize={18}>What To Look For While Buying A Chair</Text>
+                    </Box>
+                    <Box>
+                        <img width={"95%"} src="https://ii3.pepperfry.com/media/wysiwyg/banners/hp_need_help_p_6.jpg" alt="" />
+                    </Box>
+                </Box>
+            </Box>
+
+            <Box marginTop={"30px"} paddingTop={"50px"} lineHeight={"30px"} bg={"#f3f5f7"} display={"flex"} h={"400px"}  justifyContent={"space-evenly"} textAlign={"start"}>
+                <Box>
+                <Text color={"black"} fontWeight={500}  fontSize={18}>Useful Links</Text>
+                <Text color={"grey"}  fontSize={15}>About Us</Text>
+                <Text color={"grey"}  fontSize={15}>Our Blog</Text>
+                <Text color={"grey"}  fontSize={15}>Careers</Text>
+                <Text color={"grey"}  fontSize={15}>Corporate Governance</Text>
+                <Text color={"grey"}  fontSize={15}>Pepperfry In the News</Text>
+                <Text color={"grey"}  fontSize={15}>Find A Studio</Text>
+                <Text color={"grey"}  fontSize={15}>Gift Cards</Text>
+                <Text color={"grey"}  fontSize={15}>Brands</Text>
+                <Text color={"grey"}  fontSize={15}>Customer Reviews</Text>
+                </Box>
+                <Box>
+                <Text color={"black"} fontWeight={500}  fontSize={18}>Partners</Text>
+                <Text color={"grey"}  fontSize={15}>Sell With Us</Text>
+                <Text color={"grey"}  fontSize={15}>Become a Franchisee</Text>
+                <Text color={"grey"}  fontSize={15}>Become a Pep Homie</Text>
+                <Text color={"grey"}  fontSize={15}>Design For Us</Text>
+                <Text color={"grey"}  fontSize={15}>Pepperfry In the News</Text>
+                <Text color={"grey"}  fontSize={15}>Pepperfry Marketplace</Text>
+                <Text color={"grey"}  fontSize={15}>Login to Your Merchant</Text>
+                <Text color={"grey"}  fontSize={15}>Important : GST and You</Text>
+                <Text color={"grey"}  fontSize={15}>Corporate Enquiries</Text>
+                </Box>
+                <Box>
+                <Text color={"black"} fontWeight={500}  fontSize={18}>Need Help?</Text>
+                <Text color={"grey"}  fontSize={15}>Contact Us</Text>
+                <Text color={"grey"}  fontSize={15}>Returns & Refund</Text>
+                <Text color={"grey"}  fontSize={15}>Track Your Order</Text>
+                <Text color={"grey"}  fontSize={15}>FAQs</Text>
+                <Text color={"grey"}  fontSize={15}>Buy on Phone</Text>
+                </Box>
+                <Box>
+                <Text color={"black"} fontWeight={500}  fontSize={18}>Download App</Text>
+                    <Button margin={"10px 0px 10px 0px"} variant={"outline"} borderColor={"black"} borderRadius={"0px"} >App Store</Button>
+                    <br/>
+                    <Button variant={"outline"} borderColor={"black"} borderRadius={"0px"} >Play Store</Button>
+                </Box>
+            </Box>
+
         </>
     );
 }
